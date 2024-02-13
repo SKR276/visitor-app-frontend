65cb7daba66352fdd7b790e1fe5ff1c9db5fc73c
SKR276 <sreeponnu06@gmail.com>
2024-02-13 17:49:09 +0530

    visitor-app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MainPage());
+}
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
